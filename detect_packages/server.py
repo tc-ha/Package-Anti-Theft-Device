146e838ac9bb6fbf31d205ef98543d5193a69386
taechan ha <hataeck@gmail.com>
2021-11-17 13:01:21 +0900

    Update server.py

--- detect_packages/server.py
+++ detect_packages/server.py
@@ -6,14 +6,14 @@ import detect
 
 API_KEY = open('api_key.txt').readline().split(':')[1].strip()
 
-project_id = "fluid-terminal-309613"
-model_id = "IOD8333073282588737536"
+project_id = PROJECT_ID
+model_id = MODEL_ID
 
 config = {
     "apiKey": API_KEY,
-    "authDomain": "theft-detection-675d4.firebaseapp.com",
-    "databaseURL": "theft-detection-675d4-default-rtdb.firebaseio.com",
-    "storageBucket": "theft-detection-675d4.appspot.com"
+    "authDomain": URL_DOMAIN,
+    "databaseURL": URL_DB,
+    "storageBucket": URL_BUCKET,
 }
 
 firebase = Firebase(config)
@@ -39,42 +39,3 @@ def detect_object():
 if __name__ == '__main__':
     app.run(host='192.168.45.253', port=10025)
     #app.run(host='192.168.1.8', port=8080)
-
-
-# ================================================================================
-# def localize_objects():
-#     a = request.args.get('file_name')
-#     storage.child("package_list/" + str(a)).download(str(a))
-#     #path = 'https://firebasestorage.googleapis.com/v0/b/theft-detection-675d4.appspot.com/o/test%2F20181025_135614.jpeg?alt=media&token=b5f4d665-2e1d-4bf8-9fc3-e33bb646f5b0'
-#     path = str(a)
-#     client = vision.ImageAnnotatorClient()
-
-#     with open(path, 'rb') as image_file:
-#         content = image_file.read()
-#     image = vision.Image(content=content)
-
-#     objects = client.object_localization(
-#         image=image).localized_object_annotations
-
-#     d = {}
-#     i = 0
-#     for object_ in objects:
-#         if object_.name in d.keys():
-#             if object_.score > d[object_.name]:
-#                 d[object_.name] = object_.score
-#         else:
-#             d[object_.name] = object_.score
-
-#     s = {'result': 0}
-#     x = ["Box", "Packaged goods", "Shipping box"]
-
-#     flag = 0
-#     for k, v in d.items():
-#         if k in x:
-#             flag = 1
-#             s[k] = v
-
-#     if flag == 1:
-#         s['result'] = 1
-
-#     return s
